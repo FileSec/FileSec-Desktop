0690d75ed2c5a6a40d47521924e2b2387a8ca62a
RedbackThomson <redbackthomson@gmail.com>
2016-11-12 08:40:59 -0800

    Added login functionality

--- dist/app/home/home.ctrl.js
+++ dist/app/home/home.ctrl.js
@@ -5,6 +5,9 @@
   controller('HomeController', ['$scope', 'FirebaseService', HomeController]);
 
   function HomeController($scope, FirebaseService) {
-    $scope.name = "FileSec-Desktop";
+    $scope.upload = function() {
+      var file = document.getElementById('fileUpload').files[0];
+      //Begin sending the file
+    }
   }
 })();
--- dist/app/login/login.ctrl.js
+++ dist/app/login/login.ctrl.js
@@ -2,9 +2,28 @@
   'use strict';
 
   angular.module('FileSec-Home').
-  controller('LoginController', ['$scope', 'FirebaseService', LoginController]);
+  controller('LoginController', ['$scope', '$state', 'FirebaseService', LoginController]);
 
-  function LoginController($scope, FirebaseService) {
+  function LoginController($scope, $state, FirebaseService) {
     $scope.name = "FileSec-Desktop";
+    $scope.loginCreds = {
+      email: "redbackthomson@gmail.com",
+      password: "password"
+    };
+
+    $scope.login = function() {
+      firebase.auth().signInWithEmailAndPassword($scope.loginCreds.email, $scope.loginCreds.password)
+      .then(function(user) {
+        console.log("Signed in!");
+        $state.go('home');
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.error(error);
+      });
+    };
   }
 })();
